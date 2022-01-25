e3ea7d858a310cf5620466be0597bfcca247a437
hyapp <hys4pp@gmail.com>
2022-01-24 20:51:14 -0500

    bug fixed

--- app/bl/roomsManager.js
+++ app/bl/roomsManager.js
@@ -8,7 +8,7 @@ const roomList = new RoomList();
 const mainRoom = new Room(consts.MAIN_ROOM_UID, 1000, "اتاق اصلی", 100, consts.GENDER_MIXED, ['https://i.pravatar.cc/150?img=' + Math.random(), 'https://i.pravatar.cc/150?img=' + Math.random(), 'https://i.pravatar.cc/150?img=' + Math.random()])
 
 //////////////////////////
-RoomDao.put(mainRoom, []).catch(err => { console.log(err);})
+RoomDao.put(mainRoom).catch(err => { console.log(err);})
 RoomDao.findAll().then(rooms=>{
     for (r of rooms) {
         let room = new Room(r.uid, r.members.length, r.name, 0, r.gender, r.avatars);
